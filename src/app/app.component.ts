ae97d337fbc5edf15f21b886ad33f5bbdf15dc00
XYKbear <flashbear@126.com>
2017-07-23 23:02:51 +0800

    add progress bar handle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, NavigationStart } from "@angular/router";
+
 import { UtilService } from "app/shared/util.service";
+
+import 'rxjs/add/operator/find';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor (private utilService: UtilService) {
+export class AppComponent implements OnInit {
+  constructor(
+    private utilService: UtilService,
+    private routerService: Router
+  ) {
     utilService.updateTitle('App');
-   }
+  }
 
+  onPageLoading: boolean = false;
 
+  ngOnInit() {
+    this.routerService.events.find(e => e instanceof NavigationStart).subscribe(e => {
+      this.onPageLoading = true;
+      console.log(e.toString());
+    });
+  }
 }
