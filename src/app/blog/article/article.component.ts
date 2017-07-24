8d60ddde2eea2a7553ac0d47163213e65a1c2fb3
XYKbear <flashbear@126.com>
2017-07-24 20:11:05 +0800

    add 404 page

--- src/app/blog/article/article.component.ts
+++ src/app/blog/article/article.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
-  templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  templateUrl: './article.component.html'
 })
 export class ArticleComponent implements OnInit {
 
